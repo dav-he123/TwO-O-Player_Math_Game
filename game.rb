eeeaf74d15063e08e2065fa29f69c801407cf3d6
David <david.he07@yahoo.ca>
2020-05-25 22:20:08 -0400

    changed code for game file

--- game.rb
+++ game.rb
@@ -2,3 +2,16 @@ require './main'
 
 
 def game(player)
+
+    choice_player = gets.chomp
+
+    if player_choice == answer
+      puts "#{player.name}: Yes, you are correct!!"
+    else
+      puts "#{player.name}: No, you are wrong!!"
+    end
+
+    def questions_generator 
+      @question = Question.new
+      puts "#{player.name}: What does #{number_1} plus #{number_2} equal?"
+    end
