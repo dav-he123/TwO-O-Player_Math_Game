d35b879432afb57a2d510e424dd7e79236de09e0
David <david.he07@yahoo.ca>
2020-05-25 21:16:37 -0400

    updated files and included plan for the game

---
+++ game_plan.rb
@@ -0,0 +1,51 @@
+Nouns
+
+- score of player1 & player2
+- display questions & input answers
+- wait for message for other player
+- player1 & player2 take turns answering questions
+- message displays game is over
+- method to determine winner(who ran out of lives)
+
+
+Files Needed
+
+#Main
+- contains questions
+- create a object containing questions as well as answers (generate questions)
+
+#Players
+- creates the player1 & player2
+- stores lives of player1 & player2
+
+#Game 
+- display lives of player1 & player2
+- display scores of player1 & player2 after answering question
+- asks questions to player1 & player2
+
+
+
+
+Classes Needed
+
+- player (inside player file)
+- game (inside game file)
+- turn (inside game file)*plan it out
+
+
+
+Player Class (inside Players file)
+- creates player
+- stores lives
+- players name
+
+
+Question Class (inside Game file)
+- create object containing questions & answers
+- has an addition message as question
+
+
+
+
+
+
---
+++ players.rb
@@ -0,0 +1,10 @@
+#creates the player1 & player2
+#stores lives of player1 & player2
+#contains class of player
+
+
+class Player
+
+  
+
+
